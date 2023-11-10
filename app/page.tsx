b52ecad3c830ff71f02679ad853f2b698ec2be94
Kyosuke Uetsuki <kyosukeuetsuki@gmail.com>
2023-11-10 18:49:12 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "@/app/ui/home.module.css";
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
 
 import Link from "next/link";
@@ -13,11 +14,15 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape}></div>
-          <strong>Welcome to Acme.</strong> This is the example for the{" "}
-          <a href="https://nextjs.org/learn/" className="text-blue-500">
-            Next.js Learn Course
-          </a>
-          , brought to you by Vercel.
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <a href="https://nextjs.org/learn/" className="text-blue-500">
+              Next.js Learn Course
+            </a>
+            , brought to you by Vercel.
+          </p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -33,13 +38,6 @@ export default function Page() {
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt="Screenshot of the dashboard project showing mobile version"
           />
         </div>
       </div>
